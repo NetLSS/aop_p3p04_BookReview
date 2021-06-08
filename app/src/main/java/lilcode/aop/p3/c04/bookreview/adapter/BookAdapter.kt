74b7053fb288d8b54c960c42e2f22b44646dd39e
NetLSS <futureprogrammer@naver.com>
2021-06-08 16:36:37 +0900

    Feat: 북 어뎁터 구현 (뷰바인딩 사용)

---
+++ app/src/main/java/lilcode/aop/p3/c04/bookreview/adapter/BookAdapter.kt
@@ -0,0 +1,44 @@
+package lilcode.aop.p3.c04.bookreview.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import lilcode.aop.p3.c04.bookreview.databinding.ItemBookBinding
+import lilcode.aop.p3.c04.bookreview.model.Book
+
+class BookAdapter : ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) {
+
+    // 뷰 바인딩 (item_book.xml)
+    inner class BookItemViewHolder(private val binding: ItemBookBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(bookModel: Book){
+            binding.titleTextView.text = bookModel.title
+        }
+    }
+
+    // 미리 만들어진 뷰 홀더가 없을 경우 생성하는 함수.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookItemViewHolder {
+        return BookItemViewHolder(ItemBookBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    // 실제 뷰 홀더가 뷰에 그려지게 됬을 때 데이터를 바인드하게 되는 함수.
+    override fun onBindViewHolder(holder: BookItemViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object{
+        // 같은 값이 있으면 할당 해줄 필요 없다
+        val diffUtil = object: DiffUtil.ItemCallback<Book>(){
+            override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+        }
+    }
+}
