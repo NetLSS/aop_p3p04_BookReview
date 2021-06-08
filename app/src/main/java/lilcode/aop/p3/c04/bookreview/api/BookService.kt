233b7b0983218012316a6d61664c1f43dccd38ec
NetLSS <futureprogrammer@naver.com>
2021-06-08 19:18:33 +0900

    Feat: add comment

--- app/src/main/java/lilcode/aop/p3/c04/bookreview/api/BookService.kt
+++ app/src/main/java/lilcode/aop/p3/c04/bookreview/api/BookService.kt
@@ -9,12 +9,15 @@ import retrofit2.http.Query
 interface BookService {
     // get : 데이터 요청 시 반환 http
     // post : http body에 넣어 전달
+
+    // 책 검색.
     @GET("/api/search.api?output=json")
     fun getBooksByName(
         @Query("key") apiKey: String,
         @Query("query") kweyWord: String
     ): Call<SearchBookDto>
 
+    // best seller 받아오기
     @GET("/api/bestSeller.api?output=json&categoryId=100")
     fun getBestSellerBooks(
         @Query("key") apiKey: String,
