2b4e8301efb645663ccf9afb9869cd1736e59dc6
NetLSS <futureprogrammer@naver.com>
2021-06-09 13:05:39 +0900

    Feat: Room 데이터 베이스 마이그레이션 적용

--- app/src/main/java/lilcode/aop/p3/c04/bookreview/AppDatabase.kt
+++ app/src/main/java/lilcode/aop/p3/c04/bookreview/AppDatabase.kt
@@ -1,7 +1,11 @@
 package lilcode.aop.p3.c04.bookreview
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import lilcode.aop.p3.c04.bookreview.dao.HistoryDao
 import lilcode.aop.p3.c04.bookreview.dao.ReviewDao
 import lilcode.aop.p3.c04.bookreview.model.History
@@ -12,4 +16,23 @@ abstract class AppDatabase : RoomDatabase(){
     abstract fun historyDao(): HistoryDao
     abstract fun reviewDao(): ReviewDao
 
+}
+
+fun getAppDatabase(context: Context): AppDatabase {
+
+    val migration_1_2 = object : Migration(1,2){
+        override fun migrate(database: SupportSQLiteDatabase) {
+
+            database.execSQL("CREATE TABLE `REVIEW` ('id' INTEGER, `review` TEXT," + "PRIMARY KEY(`id`))")
+        }
+
+    }
+
+    return Room.databaseBuilder(
+        context,
+        AppDatabase::class.java,
+        "BookSearchDB"
+    )
+        .addMigrations(migration_1_2)
+        .build()
 }
